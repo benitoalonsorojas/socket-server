7ebeb4a1b26189d5329473cae6c76796128540c1
Alonso Rojas <benito.alonso.rojas@gmail.com>
2023-01-08 00:27:30 -0500

    socket con funciones básicas

--- class/server.ts
+++ class/server.ts
@@ -1,16 +1,50 @@
 import express from 'express';
 import { SEVER_PORT } from '../global/environment';
+import socketIO from 'socket.io';
+import http from "http";
+import * as socket from "../socket/socket";
+
 
 export default class Server{
+  private static _instance: Server;
+
   public app: express.Application;
   public port: number;
 
-  constructor(){
+  public io: socketIO.Server;
+  private httpServer:http.Server;
+
+  private constructor(){
     this.app = express();
     this.port = SEVER_PORT;
+
+    this.httpServer = new http.Server( this.app );
+    this.io=require("socket.io")(this.httpServer, {
+      cors: {
+          origin: true,
+          credentials: true
+        },            
+    });
+
+    this.escucharSocket();
+  }
+
+  public static get instance() : Server {
+    return this._instance || ( this._instance = new this() );
+  }
+
+  private escucharSocket(){
+    console.log(`Escuchando conexiones - sockets`);
+    this.io.on('connect', client => {
+      console.log(`Cliente conectado`);
+
+      socket.mensaje(client,this.io);
+      socket.desconectar(client);
+      
+    }); 
   }
 
   start( callback: Function ){
-    this.app.listen( this.port, callback );
+    this.httpServer.listen( this.port, callback() );
   }
 }
--- index.ts
+++ index.ts
@@ -3,14 +3,14 @@ import router from "./routes/routes";
 import bodyparser from "body-parser";
 import cors from "cors";
 
-const server = new Server();
+const server = Server.instance;
 
 //BodyParser
 server.app.use( bodyparser.urlencoded({ extended: true }) );
 server.app.use( bodyparser.json() );
 
 //CORS
-server.app.use( cors({ origin: true, credentials: true}))
+//server.app.use( cors({ origin: true, credentials: true}))
 
 server.app.use('/',router);
 
